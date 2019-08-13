3718d79fd34ff0629af9d7e959f9b4eb87a61fa2
MangoJavaScript <615381799@qq.com>
2019-08-13 22:45:21 +0800

    测试2：新建仓库后提交代码到陈皓GitHub仓库

--- src/test/java/org/hello.java
+++ src/test/java/org/hello.java
@@ -4,5 +4,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("gg");
         System.out.println("提交到陈皓的本地仓库");
+        System.out.println("提交方法：在gitHub上面新建一个仓库。");
     }
 }
